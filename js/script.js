aae90ed50a5b670b8aa2563bf2fe0d1122d63c8e
Sergey <seregaan.serega2003@gmail.com>
2023-05-29 21:43:33 +0500

    first-test

--- js/script.js
+++ js/script.js
@@ -1,2 +1,18 @@
-
-
+const burger = document.querySelector(".burger");
+const navbar = document.querySelector(".header-nav");
+const line1 = document.querySelector(".l1");
+const line2 = document.querySelector(".l2");
+const line3 = document.querySelector(".l3");
+burger.addEventListener("click", () => {
+  navbar.classList.toggle("enable");
+  if (navbar.classList.contains("enable")) {
+    line1.style.transform = "rotate(45deg) translate(8px,7px)";
+    line2.style.opacity = "0";
+    line3.style.transform = "rotate(-45deg) translate(9px,-8px)";
+    
+  } else {
+    line2.style.opacity = "1";
+    line1.style.transform = "rotate(0)";
+    line3.style.transform = "rotate(0)";
+  }
+});
